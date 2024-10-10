07cc6ca37f66f43b70108b445cffa23e792bcf2e
Jonas Kromer <jonas.kromer@htwg-konstanz.de>
2024-10-10 11:34:49 +0200

    vor umbau & restrukturierung

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,14 +5,25 @@ import scala.io.StdIn.readLine
 @main def hello(): Unit =
   println("This is TUI-Blackjack")
 
-  val name = readLine("Enter you Name: ")
-
+  val name = readLine("Enter you Name: \n--> ")
   println(s"Welcome $name!")
 
   val cardDeck = Deck(Nil).createShuffledDeck()
 
   println("Your First Card:")
   drawCardsFromDeck(cardDeck, 1)
+
+  var input = readLine("To draw a card press `d`, to stop the game press `q`\n--> ")
+  while(input != "q")
+
+    if (input == "d")
+      drawCardsFromDeck(cardDeck, 1)
+
+    input = readLine("Continue by pressing `d` or quit by pressing `q`\n--> ")
+
+
+  println(s"Goodbye $name")
+
 
 
 enum Suit:
@@ -37,49 +48,45 @@ enum Rank(val value: Int):
 case class Card(rank: Rank, suit: Suit):
   override def toString = s"$rank of $suit, Value: ${rank.value}"
 
-case class Deck(cards: List[Card]) {
+case class Deck(cards: List[Card]):
   // Method to create a shuffled deck with an optional seed
-  def createShuffledDeck(seed: Option[Long] = None): Deck = {
+  def createShuffledDeck(seed: Option[Long] = None): Deck =
     val allCards = for {
       suit <- List(Suit.Spades, Suit.Hearts, Suit.Diamonds, Suit.Clubs)
       rank <- Rank.values.toList
     } yield Card(rank, suit)
 
     // Create a Random instance with the provided seed
-    val random = seed match {
+    val random = seed match
       case Some(s) => new Random(s)  // Create a Random object with the given seed
       case None    => new Random()    // Create a Random object without a seed
-    }
+
 
     Deck(random.shuffle(allCards))  // Shuffle the cards and return a new Deck
-  }
 
   // Function to draw a card from the deck
-  def draw: Option[(Card, Deck)] = cards match {
+  def draw: Option[(Card, Deck)] = cards match
     case Nil => None  // If deck is empty, return None
     case head :: tail => Some(head, Deck(tail))  // Draw the top card and return it with the rest of the deck
-  }
-}
+
+
 
 // Function to draw a number of cards from the deck
-def drawCardsFromDeck(deck: Deck, numberOfCards: Int): Unit = {
+def drawCardsFromDeck(deck: Deck, numberOfCards: Int): Unit =
   var currentDeck = deck // Use a mutable variable to keep track of the current deck
-
-  println("Drawing cards from the deck:")
-
-  for (i <- 1 to numberOfCards) {
+  for (i <- 1 to numberOfCards)
     currentDeck.draw match {
       case Some((card, remainingDeck)) =>
-        println(s"Drawn Card: $card")
+        println(s"\nDrawn Card: $card")
         currentDeck = remainingDeck // Update the current deck to the remaining deck
       case None =>
-        println("No cards left in the deck.")
+        println("\nNo cards left in the deck.")
         scala.util.boundary // Exit the loop if there are no more cards
     }
-  }
 
-  println(s"Cards remaining in the deck: ${currentDeck.cards.size}")
-}
+
+  println(s"Cards remaining in the deck: ${currentDeck.cards.size}\n")
+  // deck = currentDeck
 
 case class Hand(cards: List[Card]):
   // Method to calculate total hand value
@@ -89,10 +96,10 @@ case class Hand(cards: List[Card]):
       case ((aCount, total), Card(Rank.Ace, _)) => (aCount + 1, total + Rank.Ace.value)
       case ((aCount, total), card) => (aCount, total + card.rank.value)
     }
+
     // Subtract one ace if total value is over 21
-    if (initialValue > 21) {
+    if (initialValue > 21)
       return initialValue - 10
-    }
 
     initialValue // Return if no ace spawned
 
