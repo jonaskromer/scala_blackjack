19f4f6d58099392ea8cfd1bfad2e64077d8247c8
Jonas Kromer <jonas.kromer27@gmail.com>
2024-11-13 20:36:16 +0100

    adapted to new architecture

--- src/test/scala/de/htwg/se/blackjack/model/GameSpec.scala
+++ src/test/scala/de/htwg/se/blackjack/model/GameSpec.scala
@@ -1,7 +1,10 @@
 package de.htwg.se.blackjack.model
 
+import de.htwg.se.blackjack.control.Controller
 import de.htwg.se.blackjack.model.{Card, Deck, Game, Hand, Player, Rank, Suit}
-import model.*
+import de.htwg.se.blackjack.model.*
+import de.htwg.se.blackjack.model.state.{DealerState, DistributeState, EvaluateState, FinishedState, InitState}
+import de.htwg.se.blackjack.view.Tui
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -15,7 +18,7 @@ class GameSpec extends AnyWordSpec {
       val overbuyingPlayer = Player("Alice", Hand(List(Card(Rank.Ten, Suit.Clubs), Card(Rank.Jack, Suit.Hearts), Card(Rank.Three, Suit.Spades))))
       val otherPlayer = Player("Bob", Hand(List(Card(Rank.Two, Suit.Diamonds))))
       val deck = Deck(List(Card(Rank.Four, Suit.Clubs)))
-      val game = Game(List(overbuyingPlayer, otherPlayer), 0, deck, DISTRIBUTE)
+      val game = Game(List(overbuyingPlayer, otherPlayer), 0, deck, DistributeState())
 
       val outputCapture = new ByteArrayOutputStream()
       Console.withOut(new PrintStream(outputCapture)) {
@@ -30,34 +33,34 @@ class GameSpec extends AnyWordSpec {
     "allow the current player to hit and draw a card" in {
       val initialDeck = Deck(List(Card(Rank.Ten, Suit.Clubs), Card(Rank.Six, Suit.Hearts)))
       val player = Player("Alice", Hand(List.empty))
-      val initialGame = Game(List(player), 0, initialDeck, DISTRIBUTE)
+      val initialGame = Game(List(player), 0, initialDeck, DistributeState())
       val gameAfterHit = initialGame.hit()
 
       gameAfterHit.players.head.hand.cards should not be empty
     }
 
     "start in the INIT state with only the dealer" in {
-      val initialGame = Game(List(Player("DEALER", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), INIT)
+      val initialGame = Game(List(Player("DEALER", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), InitState())
 
-      initialGame.state shouldBe INIT
+      initialGame.state shouldBe InitState()
     }
 
     "add a player correctly" in {
-      val initialGame = Game(List(Player("DEALER", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), INIT)
+      val initialGame = Game(List(Player("DEALER", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), InitState())
       val gameWithPlayer = initialGame.addPlayer("Alice")
 
       gameWithPlayer.players.length shouldBe 2
     }
 
     "transition to DISTRIBUTE state when the game starts" in {
-      val initialGame = Game(List(Player("DEALER", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), INIT)
+      val initialGame = Game(List(Player("DEALER", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), InitState())
       val startedGame = initialGame.startGame()
 
-      startedGame.state shouldBe DISTRIBUTE
+      startedGame.state shouldBe DistributeState()
     }
 
     "transition to DEALER when all players finished" in {
-      var game = Game(List(Player("DEALER", Hand(List.empty)),Player("Bob", Hand(List.empty))), 1, Deck(List.empty).createShuffledDeck(), INIT)
+      var game = Game(List(Player("DEALER", Hand(List.empty)),Player("Bob", Hand(List.empty))), 1, Deck(List.empty).createShuffledDeck(), InitState())
       game = game.stand()
 
       game.currentPlayer shouldBe 0
@@ -65,60 +68,66 @@ class GameSpec extends AnyWordSpec {
 
     "allow the current player to draw a card" in {
       val deck = Deck(List(Card(Rank.Five, Suit.Hearts), Card(Rank.Seven, Suit.Clubs)))
-      val initialGame = Game(List(Player("Alice", Hand(List.empty)), Player("Bob", Hand(List.empty))), 0, deck, DISTRIBUTE)
+      val initialGame = Game(List(Player("Alice", Hand(List.empty)), Player("Bob", Hand(List.empty))), 0, deck, DistributeState())
       val gameAfterDraw = initialGame.drawCard(initialGame.players.head, deck)
 
       gameAfterDraw.deck.cards.length shouldBe 1
     }
 
     "move to the next player after stand" in {
-      val initialGame = Game(List(Player("DEALER", Hand(List.empty)), Player("Alice", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), DISTRIBUTE)
+      val initialGame = Game(List(Player("DEALER", Hand(List.empty)), Player("Alice", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), DistributeState())
       val gameAfterStand = initialGame.stand()
 
       gameAfterStand.currentPlayer shouldBe 1
     }
 
     "switch to DEALER state when the dealer’s turn begins" in {
-      val gameWithDealerTurn = Game(List(Player("DEALER", Hand(List.empty)), Player("Alice", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), DISTRIBUTE).evalIfDealer()
+      val gameWithDealerTurn = Game(List(Player("DEALER", Hand(List.empty)), Player("Alice", Hand(List.empty))), 0, Deck(List.empty).createShuffledDeck(), DistributeState()).evalIfDealer()
 
-      gameWithDealerTurn.state shouldBe DEALER
+      gameWithDealerTurn.state shouldBe DealerState()
     }
 
     "handle dealer drawing cards until total value is 17 or higher" in {
       val deck = Deck(List(Card(Rank.Five, Suit.Hearts), Card(Rank.Six, Suit.Diamonds), Card(Rank.Seven, Suit.Spades)))
       val dealer = Player("DEALER", Hand(List.empty))
-      val initialGame = Game(List(dealer), 0, deck, DEALER)
+      val initialGame = Game(List(dealer), 0, deck, DealerState())
       val gameAfterDealerDraw = initialGame.dealerDraw()
 
       gameAfterDealerDraw.players.head.hand.totalValue should be >= 17
-      gameAfterDealerDraw.state shouldBe EVALUATE
+      gameAfterDealerDraw.state shouldBe EvaluateState()
     }
 
     "evaluate winners of the game and print results" in {
       val dealer = Player("DEALER", Hand(List(Card(Rank.Ten, Suit.Clubs), Card(Rank.Six, Suit.Spades))))
       val player = Player("Alice", Hand(List(Card(Rank.Two, Suit.Diamonds), Card(Rank.Three, Suit.Hearts))))
-      val gameToEvaluate = Game(List(dealer, player), 0, Deck(List.empty).createShuffledDeck(), EVALUATE)
+      val gameToEvaluate = Game(List(dealer, player), 0, Deck(List.empty).createShuffledDeck(), EvaluateState())
       val evaluatedGame = gameToEvaluate.evalGame()
 
-      evaluatedGame.state shouldBe FINISHED
+      evaluatedGame.state shouldBe FinishedState()
       evaluatedGame.players(1).hand.totalValue shouldBe 5
     }
 
     "evaluate loosers of the game and print results" in {
       val dealer = Player("DEALER", Hand(List(Card(Rank.Ten, Suit.Clubs), Card(Rank.Six, Suit.Spades), Card(Rank.Ten, Suit.Hearts))))
       val player = Player("Alice", Hand(List(Card(Rank.Ten, Suit.Diamonds), Card(Rank.Ten, Suit.Hearts), Card(Rank.Ten, Suit.Hearts))))
-      val gameToEvaluate = Game(List(dealer, player), 0, Deck(List.empty).createShuffledDeck(), EVALUATE)
+      val gameToEvaluate = Game(List(dealer, player), 0, Deck(List.empty).createShuffledDeck(), EvaluateState())
       val evaluatedGame = gameToEvaluate.evalGame()
 
-      evaluatedGame.state shouldBe FINISHED
+      evaluatedGame.state shouldBe FinishedState()
     }
 
     "restart the game with only the dealer and a new shuffled deck" in {
-      val initialGame = Game(List(Player("Alice", Hand(List.empty)), Player("Bob", Hand(List.empty))), 1, Deck(List.empty).createShuffledDeck(), FINISHED)
-      val restartedGame = initialGame.restart()
+      val initialGame = Game(List(Player("Alice", Hand(List.empty)), Player("Bob", Hand(List.empty))), 1, Deck(List.empty).createShuffledDeck(), FinishedState())
+      val restartedGame = initialGame.setup()
 
-      restartedGame.state shouldBe INIT
+      restartedGame.state shouldBe InitState()
       restartedGame.deck.cards.length shouldBe 52
+    }
+
+    "should have correct string representation" in {
+      val game = Game(List.empty, 0, Deck(List.empty).createShuffledDeck(), DistributeState())
+
+      todo
     }
   }
 }
